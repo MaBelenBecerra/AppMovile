85d307fc2134d5bfb8144fc6398ba7589691baaa
Maria Belen Becerra <belustianabecerra@gmail.com>
2025-09-05 23:09:17 -0400

    Datos2

--- Hallo.kt
+++ Hallo.kt
@@ -3,4 +3,19 @@ fun main() {
     val nombreMateria = "Aplicaciones Moviles"
 
     println(" $nombreCompleto y $nombreMateria.")
+    
+    var peso: Double = 60
+	println("Mi peso: $peso kg")
+
+	var altura: Double = 1.69
+	println("Mi altura: $altura m")
+
+	var edad: Int = 21
+	println("Mi edad: $edad años")
+
+	var esEdadPar: Boolean = edad % 2 == 0
+	println("¿Mi edad es par?: $esEdadPar")
+
+	var genero: String = "Femenino"
+	println("Mi genero: $genero")
 }
