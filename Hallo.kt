3d9e0a42f5ce6ee3d50a437cc75716353b7ed07b
Maria Belen Becerra <belustianabecerra@gmail.com>
2025-09-05 23:18:13 -0400

    Funciones

--- Hallo.kt
+++ Hallo.kt
@@ -1,3 +1,37 @@
-fun main() {
-    println("Hallo, Welt!!!")
+fun saludarPersona(saludo: String = "Hola", nombreCompleto: String) {
+    println("$saludo, $nombreCompleto")
+    
 }
+
+fun obtenerMensajeGenero(genero: String): String {
+    return "Mi genero es $genero"
+}
+
+fun main() {
+    val nombreCompleto = "Maria Belen Becerra Rivera"
+    val nombreMateria = "Aplicaciones Moviles"
+
+    println("$nombreCompleto, $nombreMateria.")
+    
+    var peso: Int = 60
+	println("Mi peso: $peso kg")
+
+	var altura: Double = 1.69
+	println("Mi altura: $altura m")
+
+	var edad: Int = 21
+	println("Mi edad: $edad años")
+
+	var esEdadPar: Boolean = edad % 2 == 0
+	println("¿Mi edad es par?: $esEdadPar")
+
+	var genero: String = "Femenino"
+	println("Mi genero: $genero")
+    
+    
+    saludarPersona(nombreCompleto = "Maria Belen Becerra Rivera")
+
+	val mensajeGenero = obtenerMensajeGenero("Femenino")
+	println(mensajeGenero)
+}
+
